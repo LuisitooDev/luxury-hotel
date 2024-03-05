c9f5fd4cb2214158287bfa85286125846a137dcd
LuisitooDev <laha11590@hotmail.com>
2024-03-05 15:07:08 -0700

    añadido el admin edit

--- src/pages/admin/tablaReservaciones.jsx
+++ src/pages/admin/tablaReservaciones.jsx
@@ -20,7 +20,8 @@ function Tabla_Reservaciones() {
   }, []);
 
   const getReservations = () => {
-    axios.get("http://localhost:3001/getReservations")
+    axios
+      .get("http://localhost:3001/getReservations")
       .then((response) => {
         setReservations(response.data);
       })
@@ -30,12 +31,13 @@ function Tabla_Reservaciones() {
   };
 
   const add = () => {
-    axios.post("http://localhost:3001/createReservation", {
-      fecha_llegada: fecha_llegada,
-      fecha_salida: fecha_salida,
-      total_pago: total_pago,
-      id_habitacion: habitacion,
-    })
+    axios
+      .post("http://localhost:3001/createReservation", {
+        fecha_llegada: fecha_llegada,
+        fecha_salida: fecha_salida,
+        total_pago: total_pago,
+        id_habitacion: habitacion,
+      })
       .then(() => {
         window.location.reload();
       })
@@ -45,17 +47,17 @@ function Tabla_Reservaciones() {
   };
 
   const update = () => {
-    axios.put(`http://localhost:3001/updateReservation`, {
-      id_reservacion: id_reservation,
-      fecha_llegada: fecha_llegada,
-      fecha_salida: fecha_salida,
-      total_pago: total_pago,
-      id_usuario: cliente,
-      id_habitacion: habitacion,
-    })
+    axios
+      .put(`http://localhost:3001/updateReservation`, {
+        id_reservacion: id_reservation,
+        fecha_llegada: fecha_llegada,
+        fecha_salida: fecha_salida,
+        total_pago: total_pago,
+        id_usuario: cliente,
+        id_habitacion: habitacion,
+      })
       .then(() => {
         alert("Reservacion actualizado con éxito");
-        limpiarCampos();
         setEditar(false);
         getReservations();
       })
@@ -65,7 +67,8 @@ function Tabla_Reservaciones() {
   };
 
   const deleteReservation = (idReservacion) => {
-    axios.delete(`http://localhost:3001/deleteReservation/${idReservacion}`)
+    axios
+      .delete(`http://localhost:3001/deleteReservation/${idReservacion}`)
       .then(() => {
         console.log("Reservacion eliminada con éxito");
         getReservations();
@@ -101,58 +104,141 @@ function Tabla_Reservaciones() {
     setFecha_salida(date);
   };
 
-    return (
-      <>
-          
-          <div className="overflow-x-auto min-h-screen w-full">
-      <table className="table table-primary table-hover w-full">
-        {/* head */}
-        <thead>
-          <tr>
-            <th>ID Reservación</th>
-            <th>Fecha de Llegada</th>
-            <th>Fecha de Salida</th>
-            <th>Total de Pago</th>
-            <th>Cliente</th>
-            <th>Habitación</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Mapear las reservaciones */}
-          {reservations.map((reservation) => (
-            <tr className="table-dark" key={reservation.id_reservacion}>
-              <th scope="row">{reservation.id_reservacion}</th>
-              <td>{reservation.fecha_llegada}</td>
-              <td>{reservation.fecha_salida}</td>
-              <td>${reservation.total_pago}</td>
-              <td>{reservation.id_usuario}</td>
-              <td>{reservation.id_habitacion}</td>
-              <td>
-                <div className="btn-group" role="group" aria-label="Basic example">
-                  <button
-                    type="button"
-                    onClick={() => editarReservation(reservation)}
-                    className="btn btn-warning"
-                  >
-                    Editar
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => deleteReservation(reservation.id_reservacion)}
-                    className="btn btn-danger"
-                  >
-                    Eliminar
-                  </button>
-                </div>
-              </td>
+  return (
+    <>
+      <div className="overflow-x-auto min-h-screen w-full">
+        <table className="table  bg-white text-black table-hover w-full">
+          {/* head */}
+          <thead className="text-black">
+            <tr>
+              <th>ID Reservación</th>
+              <th>Fecha de Llegada</th>
+              <th>Fecha de Salida</th>
+              <th>Total de Pago</th>
+              <th>Cliente</th>
+              <th>Habitación</th>
+              <th>Acciones</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-      </>
-    );
-  }
-  
-  export default Tabla_Reservaciones;
+          </thead>
+          <tbody>
+            {/* Mapear las reservaciones */}
+            {reservations.map((reservation) => (
+              <tr className="table-dark" key={reservation.id_reservacion}>
+                <th scope="row">{reservation.id_reservacion}</th>
+                <td>{reservation.fecha_llegada}</td>
+                <td>{reservation.fecha_salida}</td>
+                <td>${reservation.total_pago}</td>
+                <td>{reservation.id_usuario}</td>
+                <td>{reservation.id_habitacion}</td>
+                <td>
+                  <div
+                    className="btn-group"
+                    role="group"
+                    aria-label="Basic example"
+                  >
+                    <button
+                      type="button"
+                      onClick={() => {
+                        document.getElementById("edit_modal").showModal();
+                        editarReservation(reservation);
+                      }}
+                      className="btn btn-warning"
+                    >
+                      Editar
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() =>
+                        deleteReservation(reservation.id_reservacion)
+                      }
+                      className="btn btn-danger"
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <dialog id="edit_modal" className="modal">
+        <div className="modal-box w-11/12 max-w-5xl p-4">
+          <h3 className="font-bold text-lg mb-4">Edit your reservation</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <input
+              type="text"
+              onChange={(event) => setHabitacion(event.target.value)}
+              value={habitacion}
+              placeholder="Set Habitation"
+              className="input input-bordered w-full"
+            />
+            <input
+              type="text"
+              onChange={(event) => setTotal_pago(event.target.value)}
+                    value={total_pago}
+              placeholder="Set Price"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="flex flex-col">
+              <label htmlFor="arrivalDate" className="mb-2">
+                Arrival Date:
+              </label>
+              <DatePicker
+                id="arrivalDate"
+                selected={fecha_llegada}
+                onChange={handleArrivalDateChange}
+                className="w-full input input-bordered"
+                calendarContainer={(props) => (
+                  <div
+                    {...props}
+                    style={{
+                      position: "fixed",
+                      top: 0,
+                      left: 0,
+                      zIndex: 9999,
+                    }}
+                  />
+                )}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="departureDate" className="mb-2">
+                Departure Date:
+              </label>
+              <DatePicker
+                id="departureDate"
+                selected={fecha_salida}
+                onChange={handleDepartureDateChange}
+                className="w-full input input-bordered"
+                calendarContainer={(props) => (
+                  <div
+                    {...props}
+                    style={{
+                      position: "fixed",
+                      top: 0,
+                      left: 0,
+                      zIndex: 9999,
+                    }}
+                  />
+                )}
+              />
+            </div>
+          </div>
+          <div className="modal-action">
+          <button className="btn" onClick={() => update(id_reservation)}>Edit</button>
+            <form method="dialog">
+              {/* if there is a button, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
+}
+
+export default Tabla_Reservaciones;
