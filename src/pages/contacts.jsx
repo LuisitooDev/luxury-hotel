92ef7cb9d76b5167aff6783a2f3c638766ddac50
AdalCastillo <adalalancastillo@gmail.com>
2024-03-06 11:45:46 -0700

    Contactos

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -6,7 +6,7 @@ function Contacts(){
 
 
     return (
-        <>
+        <div class="bg-neutral">
             <div>
                 <Hero backgroundUrl="home.png"/>
             </div>
@@ -14,10 +14,12 @@ function Contacts(){
             <div className="divider divider-success" />
             <div class="flex">
                 <div className="w-1/2 p-4">
+                    <h2 className="mb-5 text-4xl font-bold text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
                     </svg>
-                    <h2 className="mb-5 text-4xl font-bold text-white">497 Evergreen Rd. Roseville, CA 95673</h2>
+                        497 Evergreen Rd. Roseville, CA 95673
+                    </h2>
                     <h2 className="mb-5 text-4xl font-bold text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
@@ -37,14 +39,14 @@ function Contacts(){
                     Please forward it to our support service and we will get back to you as soon as possible.
                     </p>
                     <form className="mb-5 text-white" action="https://formsubmit.co/luxuryhotel@yopmail.com" method="POST">
-                        <label class="input input-bordered flex items-center gap-2 input-success input-md w-full">
+                        <label class="input input-bordered bg-neutral flex items-center gap-2 input-success input-md w-full">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                         </svg>
                         <input type="text" class="w-full p-2 pl-3 pr-3" name="Nombre" placeholder="Ingrese su nombre" required></input><br/>
                         </label>
                         <br/>
-                        <label class="input input-bordered flex items-center gap-2 input-success input-md w-full">
+                        <label class="input input-bordered bg-neutral flex items-center gap-2 input-success input-md w-full">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                         </svg>
@@ -52,7 +54,7 @@ function Contacts(){
                         </label>
                         <br/>
                         <label>
-                            <textarea type="text" name="Mensaje" placeholder="Escriba su mensaje" class="textarea textarea-success textarea-md w-full" required /><br/>
+                            <textarea type="text" name="Mensaje" placeholder="Escriba su mensaje" class="textarea textarea-success bg-neutral textarea-md w-full" required /><br/>
                         </label>
                         <br/>
                         <button type="submit" className="btn btn-success">
@@ -64,7 +66,7 @@ function Contacts(){
                     </form>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
